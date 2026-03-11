4186fcffb4a0f3c6308111acd4cf2cac77319a9a
zinebtalghmet <zinebtalghmet2022@gmail.com>
2026-03-11 14:07:42 +0000

    NEW UP

--- sqlchallenge1.sql
+++ sqlchallenge1.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE LIBRARY ;
 CREATE TABLE library_books (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(150),
-    author VARCHAR(100),
+    title VARCHAR(150) NOT NULL,
+    author VARCHAR(100) NOT NULL,
     published_year YEAR,
     status ENUM('Available','Borrowed','Lost'),
     price DECIMAL(10.2)
