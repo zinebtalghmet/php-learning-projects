222e2845ba946f41eaa48e20ac103fd8d3ad2940
zinebtalghmet <zinebtalghmet2022@gmail.com>
2026-03-16 11:51:38 +0000

    challenge 8 pdo

---
+++ challenge8_pdo.php
@@ -0,0 +1,20 @@
+<?php
+include 'challenge7_PDO.php';
+$minPrice = 2500;
+
+$sql = "SELECT asset_id, serial_number,device_name, price, status FROM assets WHERE price > :minPrice";
+
+$statment = $pdo->prepare($sql);
+
+$statment->execute(['minPrice' =>$minPrice]);
+
+$assets =$statment->fetchAll();
+
+?>
+<ul>
+    <?php foreach ($assets as $asset) : ?>
+        <li><?= $asset['device_name'] ?> - $<?= $asset['price'] ?></li>
+    <?php endforeach; ?>
+</ul>
+
+</table>
